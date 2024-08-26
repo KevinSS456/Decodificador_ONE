a89d82d76ed52e1522a4eafaffceb0293fe68d97
KevinSS456 <kesantiago246@gmail.com>
2024-08-25 23:39:47 -0300

    Progresso do projeto

---
+++ script.js
@@ -0,0 +1,56 @@
+const Cod = document.querySelector('#criptografar')
+const Des = document.querySelector('#descriptografar')
+const Copiar = document.querySelector('#copiar');
+var resultado
+
+
+
+function criptografar(){
+    let input = document.querySelector('#texto')
+    let res = document.querySelector('#mudar')
+
+    resultado = input.value
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat")
+
+    input.value = ''
+    res.style.justifyContent = 'flex-start'
+    res.style.textAlign = 'left'
+    res.style.alignItems = 'flex-start';
+    res.innerHTML = '<div id = "resposta"><p>'+resultado+'</p></div>'
+    Copiar.style.display = 'block'
+}
+function descriptografar(){
+    let input = document.querySelector('#texto')
+    let res = document.querySelector('#mudar')
+
+    resultado = input.value
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u")
+
+    input.value = ''
+    res.style.justifyContent = 'flex-start'
+    res.style.textAlign = 'left'
+    res.style.alignItems = 'flex-start';
+    res.innerHTML = '<div id = "resposta"><p>'+resultado+'</p></div>'
+    Copiar.style.display = 'block'
+}
+function copiar(){
+
+    navigator.clipboard.writeText(resultado).then(function() {
+        alert('Texto copiado com sucesso!');
+    }).catch(function(err) {
+        console.error('Erro ao copiar o texto: ', err);
+    });
+}
+
+Cod.addEventListener('click', criptografar)
+Des.addEventListener('click', descriptografar)
+Copiar.addEventListener('click', copiar)
+
